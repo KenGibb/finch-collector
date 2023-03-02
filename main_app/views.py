1ed97bac0cfe3c83076001d51da98bfdebd01cb7
KenGibb <kenjibbo@gmail.com>
2023-03-02 15:28:31 -0600

    added index finch

--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 
+finches = [
+    {'name': 'Terry', 'species': 'American Goldfinch', 'age': '2', 'description': 'Nice gold birdy'},
+    {'name': 'Laura', 'species': 'House Finch', 'age': '4', 'description': 'Oh cool woody looking bird with a red head'},
+    {'name': 'Bill', 'species': 'Cassia Crossbill', 'age': '1', 'description': 'A bird with a red plumage'},
+]
+
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
+
+def finches_index(request):
+    return render(request, 'finches/index.html', { 'finches': finches })
